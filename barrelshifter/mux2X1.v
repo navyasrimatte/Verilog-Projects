5898bb64a19be44a775fdbdd7520b9965feef4dd
navyasrimatte <149217809+navyasrimatte@users.noreply.github.com>
2024-09-07 14:15:46 +0530

    Add files via upload

---
+++ barrelshifter/mux2X1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.06.2024 14:17:36
+// Design Name: 
+// Module Name: mux2X1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux2X1(
+
+    );
+endmodule
