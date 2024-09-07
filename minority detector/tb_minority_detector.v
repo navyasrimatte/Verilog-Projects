b99abaa1f670e27bcc3b63686cbca47baa573c3c
navyasrimatte <149217809+navyasrimatte@users.noreply.github.com>
2024-09-07 19:20:30 +0530

    Add files via upload

---
+++ minority detector/tb_minority_detector.v
@@ -0,0 +1,17 @@
+`timescale 1ns / 1ps
+
+module tb_minority_detector();
+reg a,b,c;
+wire detect;
+minority_detector  md(detect,a,b,c);
+initial 
+{a,b,c}=0;
+initial begin
+for(integer i=0;i<8;i=i+1)
+begin
+{a,b,c}=i;
+#10;
+end
+end
+endmodule
+
