87cd0f663e68283da8e7c120e16d6d43271ac7e9
navyasrimatte <149217809+navyasrimatte@users.noreply.github.com>
2024-09-07 19:29:43 +0530

    Add files via upload

---
+++ mealey sequence non overlap detector/tb_sequence_detector.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+
+module tb_sequence_detector();
+  reg clk, rst_n, x;
+  wire z;
+  
+ mealy_sequence_detector sd(clk, rst_n, x, z);
+  initial clk = 0;   
+  always #2 clk = ~clk;
+    
+  initial begin
+    x = 0;
+    #1 rst_n = 0;
+    #2 rst_n = 1;
+    
+    #3 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #10;
+    $finish;
+  end
+  
+  initial begin
+    // Dump waves
+    $dumpfile("dump.vcd");
+    $dumpvars(0);
+  end
+endmodule
+
